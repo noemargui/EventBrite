c041707f0a1ed5e842b3689b3b636d6bf34a4552
vincelep <vincent.leparoux@gmail.com>
2019-02-11 22:14:51 +0100

    vérification du second mailer

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,9 +1,11 @@
 class Attendance < ApplicationRecord
   after_create :newattendee_send
 
+# les associations
   belongs_to :attendee, class_name: "User"
   belongs_to :event
 
+# méthode pour le mailing
   def newattendee_send #Quand un participant participe à un événement, envoie un email au créateur de l'événement
     AdminMailer.newattendee_email(self).deliver_now
   end
