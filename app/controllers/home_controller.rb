d67e923b2748d9452784e69754ab65872dbda0c6
vincelep <vincent.leparoux@gmail.com>
2019-02-12 20:53:47 +0100

    modif du seed + ajout controllers + modifs views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,9 @@
 class HomeController < ApplicationController
 
-  before_action :authenticate_user!, only: [:secret]
-
   def index
   end
 
   def secret
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,19 +8,39 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.destroy_all
-Event.destroy_all
-Attendance.destroy_all
+# User.destroy_all
+# Event.destroy_all
+# Attendance.destroy_all
 
 
 10.times do
-  user = User.create!(first_name: Faker::Name.unique.first_name, last_name: Faker::Name.unique.last_name, email: Faker::Internet.unique.email, description: Faker::Lorem.characters(25))
+  user = User.create!(
+    first_name: Faker::Name.unique.first_name,
+    last_name: Faker::Name.unique.last_name,
+    #email: Faker::Internet.unique.email,
+    description: Faker::Lorem.characters(25),
+    email: "#{Faker::Name.first_name}@yopmail.com",
+    password: "Marsupilami"
+    )
 end
 
 5.times do
-  event = Event.create!(start_date: Faker::Time.forward(90, :morning), admin_id: rand(User.first.id..User.last.id), duration: (rand(1..24)*5), title: Faker::Lorem.characters(15), description: Faker::Lorem.characters(40), price: Faker::Number.number(3), location: Faker::Address.city)
+  event = Event.create!(
+    start_date: Faker::Time.forward(90, :morning),
+    admin_id: rand(User.first.id..User.last.id),
+    duration: (rand(1..24)*5),
+    title: Faker::Lorem.characters(15),
+    description: Faker::Lorem.characters(40),
+    price: Faker::Number.number(3),
+    location: Faker::Address.city
+    )
 end
 
 20.times do
-  attendance = Attendance.create!(attendee_id: rand(User.first.id..User.last.id), event_id: rand(Event.first.id..Event.last.id))
+  attendance = Attendance.create!(
+    attendee_id: rand(User.first.id..User.last.id),
+    event_id: rand(Event.first.id..Event.last.id)
+    )
 end
+
+
