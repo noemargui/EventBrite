1b2ee794c33ef6cda8ab780a3c4b1849c04dd5c4
vincelep <vincent.leparoux@gmail.com>
2019-02-12 16:14:05 +0100

    mise en page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   after_create :welcome_send
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   has_many :attendances, foreign_key: 'attendee_id'
   has_many :events, foreign_key: 'admin_id'
---
+++ db/migrate/20190212134648_remove_part_number_from_users.rb
@@ -0,0 +1,5 @@
+class RemovePartNumberFromUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :email, :string
+  end
+end
